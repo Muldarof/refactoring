2056f501625e40d36a6e55c4a74e4c91298a28ab
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-10 02:23:07 -0400

    updated index.js with json data

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 import { statement } from "./invoice";
+const invoices = [
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+]
+const plays = {
+    "hamlet": {
+        "name": "Hamlet",
+        "type": "tragedy"
+    },
+    "as-like": {
+        "name": "As You Like It",
+        "type": "comedy"
+    },
+    "othello": {
+        "name": "Othello",
+        "type": "tragedy"
+    }
+}
