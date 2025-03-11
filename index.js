d17172abb532cf81f204d9e2c2d048d7262b7c4f
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 18:30:13 -0400

    tweaked expected output changed from 40 to 47

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var plays = {
 //console.log(statement(invoices[0], plays))
 function main(invoices, plays){
     for(let invoice of invoices){
-        let expectedOutput = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 40 credits\n`
+        let expectedOutput = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`
         let statementOutput = statement(invoice, plays)
         console.log(`Expected Output:\n=============================\n${expectedOutput}\n`)
         console.log(`Actual Output:\n=============================\n${statementOutput}`)
