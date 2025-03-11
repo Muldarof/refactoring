b83db701d20ab1664976646327b5d6ad9e226696
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 04:11:48 -0400

    refactor, moved switch case to own function

--- invoice.js
+++ invoice.js
@@ -10,23 +10,7 @@ export function statement (invoice, plays){
         const play = plays[perf.playID];
         let thisAmount = 0;
 
-        switch(play.type){
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30){
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20){
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience
-                break;
-            defualt:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        thisAmount = newFunction(play, thisAmount, perf);
 
         //add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -40,4 +24,24 @@ export function statement (invoice, plays){
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`
     return result;
+
+    function newFunction(play, thisAmount, perf) {
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+                defualt: throw new Error(`unknown type: ${play.type}`);
+        }
+        return thisAmount;
+    }
 }
