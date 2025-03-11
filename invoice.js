a16d78e2c50fdb0a7185eac9a1ab354ffed010ac
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 05:29:11 -0400

    added enrichPerformance function

--- invoice.js
+++ invoice.js
@@ -1,12 +1,18 @@
 export function statement (invoice, plays){
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, invoice, plays);
+
+    function enrichPerformance(aPerformance){
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         //print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
@@ -16,7 +22,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -24,7 +30,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
