22d686c4c47de0135480090fdca4b874847ec245
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 06:12:06 -0400

    added renderHtml function and helper function

--- invoice.js
+++ invoice.js
@@ -5,15 +5,7 @@ export function statement (invoice, plays){
 
 }
 
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber / 100);
-}
-
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         //print line for this order
@@ -23,3 +15,30 @@ function renderPlainText(data, plays) {
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
 }
+
+export function htmlStatement(invoice, plays){
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data){
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += `<table>\n`
+    result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>`
+    for (let perf of data.performances){
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += `</table>\n`;
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
+}
+
+
