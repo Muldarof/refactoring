c686a5d886f78e5e08fb69ba04b87525cb2c9616
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 18:09:28 -0400

    added class PerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -7,6 +7,7 @@ export function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance){
+        const calculator = new PerformanceCalculator(aPerformance);
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result)
@@ -59,6 +60,12 @@ export function createStatementData(invoice, plays) {
             result += perf.amount;
         }
         return result;
+    }
+}
+
+class PerformanceCalculator{
+    constructor(aPerformance){
+        this.performance = aPerformance;
     }
 }
 
