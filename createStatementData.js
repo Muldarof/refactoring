47befdbf8f3af00faec3e3f5bf208c219a651d0d
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-11 19:04:56 -0400

    refactor modified logic for volume credits in base class and moved conditional into subclass

--- createStatementData.js
+++ createStatementData.js
@@ -64,10 +64,7 @@ class PerformanceCalculator{
     }
 
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -88,6 +85,10 @@ class ComdeyCalculator extends PerformanceCalculator{
         }
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
